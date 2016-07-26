816504d1cfcf0bcac5a966c4808e444fca677300
JasonThomasData <jason.robert.thomas.123@gmail.com>
2016-07-27 00:51:13 +1000

    Changed config for running on virtual terminal, doesnt support graphics.

--- config_application.py
+++ config_application.py
@@ -41,12 +41,12 @@ class ConfigDestroyTarget(ConfigCommon):
 class ConfigKillerBots(ConfigCommon):
     def __init__(self):
         ConfigCommon.__init__(self)
-        self.generations_to_process = 2000     #The number of tests to run over to generate the robot memory
+        self.generations_to_process = 3000     #The number of tests to run over to generate the robot memory
         self.random_threshold = 1      #If a random number is higher than this, do a random action - not used so far, consider removing
         self.max_moves_modifier = 1.5      #This many times larger than the test_area's area, which is test_area_width ^ 2
         self.database_location = 'database/killer_bots.db'
-        self.animate_on_multiple = 50      #Animate on the nth generation
-        self.animate_these_frames = [5, 10, 25, 75]      #Animate on the these generations
+        self.animate_on_multiple = 50000      #Animate on the nth generation
+        self.animate_these_frames = []      #Animate on the these generations
         self.animation_folder = 'animation/killer_bots/'
         self.available_actions = ['turn_left', 'turn_right', 'fire_projectile',
                                 'move_forward', 'move_left', 'move_right']
@@ -57,3 +57,4 @@ class DatabaseConfig(object):
     def __init__(self, config):
         self.robot_memory = sqlite3.connect(config.database_location, timeout=20)
         self.cursor = self.robot_memory.cursor()
+
